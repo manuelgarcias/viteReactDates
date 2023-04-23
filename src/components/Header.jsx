ca3ed750d408453de745d18ec00f83f116d076ea
manuelgarcias <manuel.garcia.s1991@gmail.com>
2023-04-23 10:18:49 -0500

    se sube el csambio en hesder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header () {
     return (
         <h1 className="font-black text-5xl text-center w-1/2 mx-auto">
-            Seguimiento pacientes {""}
+            Seguimiento pacientes {""} con GITHUB {""} 
             <span className="text-indigo-600">veterinaria</span>
         </h1>
 
